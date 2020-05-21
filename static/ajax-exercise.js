472733d605201a6327e2e026755448c008231d6a
Brittney Huttner <brittneyhuttner@gmail.com>
2020-05-21 23:27:45 +0000

    completed order melons

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -3,10 +3,10 @@
 // Part 1
 
 $('#get-fortune-button').on('click', () => {
-  $.get('/fortune', (response) => 
-    {$('#fortune-text').html(res);
+  $.get('/fortune', (response) => {
+    $('#fortune-text').html(response);
 });
-
+  });
 
 // Part 2
 
@@ -14,34 +14,35 @@ $('#weather-form').on('submit', (evt) => {
   evt.preventDefault();
 
   const formData = {
-    // TODO: select the zipcode input
-    zipcode: $('REPLACE THIS').val()
+    zipcode: $('#zipcode-field').val()
   };
 
-  // TODO: choose a request method (GET or POST) by uncommenting one of
-  // these blocks of code
-
-  // $.get('/replaceMe', formData, (response) => {
-  //   // Fill in the callback function
-  // });
-
-  // $.post('/replaceMe', formData, (response) => {
-  //   // Fill in the callback function
-  // });
+  $.get('/weather', formData, (response) => {
+    $('#weather-info').html(response.forecast);
+  });
 });
 
 
-// Part 3
+//Part 3
 
 $("#order-form").on('submit', (evt) => {
   evt.preventDefault();
 
-  // TODO: create an object to store key-value pairs that'll be sent to
-  // the server
+  const formData = {
+    melon_type: $('#melon-type-field').val(),
+    qty: $('qty-field').val()
+  };
 
-  // TODO: make a request to /order-melons
-  //
-  // In the callback function, use the response from the server to
-  // update #order-status. IMPORTANT: if the result code is 'ERROR',
-  // make it show up in red.
-});
+  $.post('/order-melons', formData, (response) => {
+    const orderStatus = $('#order-status');
+
+    if (results.code === 'ERROR') {
+      orderStatus.css('color','red');
+    } else {
+        orderStatus.css('color', '');
+      }
+
+      orderStatus.html(`<p>${response.msg}</p>`);
+    });
+  });
+
